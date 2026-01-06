5ff45c444258e84a0ab1843a0d4baddf0283635f
gksql <146406303+gganb@users.noreply.github.com>
2026-01-07 02:21:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.97 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0){
+             list.add(i);   
+            }
+        }
+        return list.stream().mapToInt(x -> x).toArray();
+    }
+}
