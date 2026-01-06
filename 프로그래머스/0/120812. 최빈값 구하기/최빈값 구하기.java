3c58e7f4e6c19cbb0b53f9719fea603198b5c745
gksql <146406303+gganb@users.noreply.github.com>
2026-01-07 02:50:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.34 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,28 +1,21 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int solution(int[] array) {
-        int[] count = new int[1000];
-        // 빈도 세기
-        for(int x : array){
-            count[x]++;
-        }
-        // 최빈값 구하기
-        int max = 0;
-        for(int y : count){
-            if(y > max){
-                max = y;
-            }
-        }
-        
-        int answer = 0;
         int maxCount = 0;
-        for(int i = 0; i < count.length; i++){
-            if(count[i] == max){
-                maxCount++;
-                answer = i;
+        int answer = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int num : array){
+            int count = map.getOrDefault(num,0) + 1;
+            if(count > maxCount){
+                maxCount = count;
+                answer = num;
+            } else if(count == maxCount){
+                answer = -1;
             }
-        }
-        if(maxCount > 1){
-            answer = -1;
+            map.put(num, count);
         }
         return answer;
     }
