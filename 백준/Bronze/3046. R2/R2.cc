ff0c05e6a1c5e6d9ffd3ca5bddb24159e2d2dad3
gksql <146406303+gganb@users.noreply.github.com>
2026-02-22 17:55:48 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <cstdlib>
+
+
+int main(int argc, char const *argv[]){
+    int r1,r2,s;
+
+    std::cin >> r1 >> s;
+    r2 = 2 * s - r1; 
+
+    std::cout << r2;
+
+    return 0;
+}
